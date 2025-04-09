71806a5bb81df64d90e32803e47e0376580f96b5
Dev-scott <sadoscott25@gmail.com>
2025-04-09 09:18:02 +0100

    work on sending email after order

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -10,82 +10,82 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST(req: Request) {
   try {
-    const body = await req.text()
-    const signature = headers().get('stripe-signature')
+    // const body = await req.text()
+    // const signature = headers().get('stripe-signature')
 
-    if (!signature) {
-      return new Response('Invalid signature', { status: 400 })
-    }
+    // if (!signature) {
+    //   return new Response('Invalid signature', { status: 400 })
+    // }
 
-    const event = stripe.webhooks.constructEvent(
-      body,
-      signature,
-      process.env.STRIPE_WEBHOOK_SECRET!
-    )
+    // const event = stripe.webhooks.constructEvent(
+    //   body,
+    //   signature,
+    //   process.env.STRIPE_WEBHOOK_SECRET!
+    // )
 
-    if (event.type === 'checkout.session.completed') {
-      if (!event.data.object.customer_details?.email) {
-        throw new Error('Missing user email')
-      }
+    // if (event.type === 'checkout.session.completed') {
+    //   if (!event.data.object.customer_details?.email) {
+    //     throw new Error('Missing user email')
+    //   }
 
-      const session = event.data.object as Stripe.Checkout.Session
+    //   const session = event.data.object as Stripe.Checkout.Session
 
-      const { userId, orderId } = session.metadata || {
-        userId: null,
-        orderId: null,
-      }
+    //   const { userId, orderId } = session.metadata || {
+    //     userId: null,
+    //     orderId: null,
+    //   }
 
-      if (!userId || !orderId) {
-        throw new Error('Invalid request metadata')
-      }
+    //   if (!userId || !orderId) {
+    //     throw new Error('Invalid request metadata')
+    //   }
 
-      const billingAddress = session.customer_details!.address
-      const shippingAddress = session.shipping_details!.address
+    //   const billingAddress = session.customer_details!.address
+    //   const shippingAddress = session.shipping_details!.address
 
-      const updatedOrder = await db.order.update({
-        where: {
-          id: orderId,
-        },
-        data: {
-          isPaid: true,
-          shippingAddress: {
-            create: {
-              name: session.customer_details!.name!,
-              city: shippingAddress!.city!,
-              country: shippingAddress!.country!,
-              postalCode: shippingAddress!.postal_code!,
-              street: shippingAddress!.line1!,
-              state: shippingAddress!.state,
-            },
-          },
+    //   const updatedOrder = await db.order.update({
+    //     where: {
+    //       id: orderId,
+    //     },
+    //     data: {
+    //       isPaid: true,
+    //       shippingAddress: {
+    //         create: {
+    //           name: session.customer_details!.name!,
+    //           city: shippingAddress!.city!,
+    //           country: shippingAddress!.country!,
+    //           postalCode: shippingAddress!.postal_code!,
+    //           street: shippingAddress!.line1!,
+    //           state: shippingAddress!.state,
+    //         },
+    //       },
        
-        },
-      })
+    //     },
+    //   })
 
-      await resend.emails.send({
-        from: 'CaseCobra <hello@joshtriedcoding.com>',
-        to: [event.data.object.customer_details.email],
-        subject: 'Thanks for your order!',
-        react: OrderReceivedEmail({
-          orderId,
-          orderDate: updatedOrder.createdAt.toLocaleDateString(),
-          // @ts-ignore
-          shippingAddress: {
-            name: session.customer_details!.name!,
-            address: shippingAddress!.address!,
+    //   await resend.emails.send({
+    //     from: 'CaseCobra <hello@joshtriedcoding.com>',
+    //     to: [event.data.object.customer_details.email],
+    //     subject: 'Thanks for your order!',
+    //     react: OrderReceivedEmail({
+    //       orderId,
+    //       orderDate: updatedOrder.createdAt.toLocaleDateString(),
+    //       // @ts-ignore
+    //       shippingAddress: {
+    //         name: session.customer_details!.name!,
+    //         address: shippingAddress!.address!,
            
-          },
-        }),
-      })
-    }
+    //       },
+    //     }),
+    //   })
+    // }
 
-    return NextResponse.json({ result: event, ok: true })
+    // return NextResponse.json({ result: event, ok: true })
   } catch (err) {
-    console.error(err)
+    // console.error(err)
 
-    return NextResponse.json(
-      { message: 'Something went wrong', ok: false },
-      { status: 500 }
-    )
+    // return NextResponse.json(
+    //   { message: 'Something went wrong', ok: false },
+    //   { status: 500 }
+    // )
   }
 }
--- src/app/dashboard/actions.ts
+++ src/app/dashboard/actions.ts
@@ -1,17 +1,47 @@
-"use server"
+"use server";
 
-import { db } from '@/db'
-import { OrderStatus } from '@prisma/client'
+import OrderReceivedEmail from "@/components/emails/OrderReceivedEmail";
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { OrderStatus } from "@prisma/client";
+import { Resend } from "resend";
 
+const resend = new Resend(process.env.RESEND_API_KEY);
 export const changeOrderStatus = async ({
   id,
   newStatus,
 }: {
-  id: string
-  newStatus: OrderStatus
+  id: string;
+  newStatus: OrderStatus;
 }) => {
-  await db.order.update({
+  const { getUser } = await getKindeServerSession();
+  const user = await getUser();
+  console.log("my user ", user);
+
+  const updatedOrder = await db.order.update({
     where: { id },
     data: { status: newStatus },
-  })
+    include: {
+      shippingAddress: true,
+    }
+  });
+
+  if(user){
+
+  await resend.emails.send({
+    from: "Frameitup <hello.dev-scott.me>",
+    to: [user.email!],
+    subject: "Thanks for your order!",
+    react: OrderReceivedEmail({
+      id,
+      orderDate: updatedOrder.createdAt.toLocaleDateString(),
+      // @ts-ignore
+      shippingAddress: {
+        name: user.family_name!,
+        address: updatedOrder.shippingAddress!.address!,
+      },
+    }),
+  });
 }
+
+};
