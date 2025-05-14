09c2e106bf59d26c1169f245f1ed56a27b38a53b
Dev-scott <sadoscott25@gmail.com>
2025-05-14 23:40:05 +0100

    work on order feature

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,13 +1,15 @@
-import { z } from 'zod'
-import { authRouter } from './auth-router'
-import { publicProcedure, router } from './trpc'
-import { QueryValidator } from '../lib/validators/query-validator'
-import { getPayloadClient } from '../get-payload'
-import { paymentRouter } from './payment-router'
+import { z } from "zod";
+import { authRouter } from "./auth-router";
+import { publicProcedure, router } from "./trpc";
+import { QueryValidator } from "../lib/validators/query-validator";
+import { getPayloadClient } from "../get-payload";
+import { paymentRouter } from "./payment-router";
+import { orderRouter } from "./order-router";
 
 export const appRouter = router({
   auth: authRouter,
   payment: paymentRouter,
+  order: orderRouter,
 
   getInfiniteProducts: publicProcedure
     .input(
@@ -15,36 +17,33 @@ export const appRouter = router({
         limit: z.number().min(1).max(100),
         cursor: z.number().nullish(),
         query: QueryValidator,
-      })
+      }),
     )
     .query(async ({ input }) => {
-      const { query, cursor } = input
-      const { sort, limit, ...queryOpts } = query
+      const { query, cursor } = input;
+      const { sort, limit, ...queryOpts } = query;
 
-      const payload = await getPayloadClient()
+      const payload = await getPayloadClient();
 
-      const parsedQueryOpts: Record<
-        string,
-        { equals: string }
-      > = {}
+      const parsedQueryOpts: Record<string, { equals: string }> = {};
 
       Object.entries(queryOpts).forEach(([key, value]) => {
         parsedQueryOpts[key] = {
           equals: value,
-        }
-      })
+        };
+      });
 
-      const page = cursor || 1
+      const page = cursor || 1;
 
       const {
         docs: items,
         hasNextPage,
         nextPage,
       } = await payload.find({
-        collection: 'products',
+        collection: "products",
         where: {
           approvedForSale: {
-            equals: 'approved',
+            equals: "approved",
           },
           ...parsedQueryOpts,
         },
@@ -52,13 +51,13 @@ export const appRouter = router({
         depth: 1,
         limit,
         page,
-      })
+      });
 
       return {
         items,
         nextPage: hasNextPage ? nextPage : null,
-      }
+      };
     }),
-})
+});
 
-export type AppRouter = typeof appRouter
+export type AppRouter = typeof appRouter;
---
+++ src/trpc/order-router.ts
@@ -0,0 +1,55 @@
+import { TRPCError } from "@trpc/server";
+import {
+  ExtendedOrderFormSchema,
+  OrderFormSchema,
+} from "../lib/validators/order-validator";
+import { privateProcedure, router } from "./trpc";
+import { getPayloadClient } from "../get-payload";
+
+export const orderRouter = router({
+  createOrder: privateProcedure
+    .input(ExtendedOrderFormSchema)
+    .mutation(async ({ ctx, input }) => {
+      const { user } = ctx;
+
+      console.log("here is the order value in the trpc router", input);
+      console.log("user", ctx);
+
+      let { productIds } = input.productIds;
+      console.log(productIds);
+
+      if (productIds.length === 0) {
+        throw new TRPCError({ code: "BAD_REQUEST" });
+      }
+
+      const payload = await getPayloadClient();
+
+      const { docs: products } = await payload.find({
+        collection: "products",
+        where: {
+          id: {
+            in: productIds,
+          },
+        },
+      });
+      console.log("list of product to order", products);
+
+      const filteredProducts = products.filter((prod) => Boolean(prod.priceId));
+      console.log("filtered products", filteredProducts);
+
+      const order = await payload.create({
+        collection: "orders",
+        data: {
+          _isPaid: false,
+          //@ts-ignore
+          products: filteredProducts.map((prod) => prod.id),
+          user: user.id,
+        },
+      });
+      console.log("order created", order);
+
+      return {
+        orderId: order.id,
+      };
+    }),
+});
