877e1e5fc8d46e07d8d861bb1821941772a78d63
Dev-scott <sadoscott25@gmail.com>
2025-05-16 01:25:39 +0100

    feature : sending email after order

--- src/trpc/order-router.ts
+++ src/trpc/order-router.ts
@@ -5,6 +5,11 @@ import {
 } from "../lib/validators/order-validator";
 import { privateProcedure, router } from "./trpc";
 import { getPayloadClient } from "../get-payload";
+import { Resend } from "resend";
+import { ReceiptEmailHtml } from "../components/emails/ReceiptEmail";
+import { Product } from "@/payload-types";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const orderRouter = router({
   createOrder: privateProcedure
@@ -51,6 +56,20 @@ export const orderRouter = router({
           },
         });
         console.log("order created", order);
+
+        const data = await resend.emails.send({
+          from: "FrameitUp <support@mail.frameitup.store>",
+          //@ts-ignore
+          to: [user.email],
+          subject: "Thanks for your order! This is your receipt.",
+          html: ReceiptEmailHtml({
+            date: new Date(),
+            //@ts-ignore
+            email: user.email,
+            orderId: order.id as string,
+            products: order.products as Product[],
+          }),
+        });
 
         return {
           url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
