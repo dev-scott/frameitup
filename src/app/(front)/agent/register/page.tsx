d491821333b44d08d41d12d988dda70d4ed0118e
Dev-scott <sadoscott25@gmail.com>
2025-06-26 18:23:29 +0100

    feaat: work on agent page

--- src/app/(front)/agent/register/page.tsx
+++ src/app/(front)/agent/register/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <MaxWidthWrapper className=" h-[60vh] mt-10  flex justify-center items-center py-5">
+      <div className=" gap-y-6 flex justify-center items-center flex-col ">
+        <span className="font-light text-center text-[14px] text-secondary">
+          Gagnez plus. Devenez un Pilier de l&apos;Art avec FrameItUp !
+        </span>
+        <h1 className="text-center font-semibold text-4xl ">
+          Rejoignez l&apos;Équipe FrameItUp et Gagnez en Partageant la Beauté.
+        </h1>
+        <p className="text-center  ">
+          As a FrameItUp agent, your role will be to showcase our unique frame
+          collections on social media, blogs, forums,
+          <br /> and to your personal network. You&apos;ll be an ambassador for
+          our brand, presenting the quality, elegance and diversity <br /> of
+          our products to an audience eager to beautify their homes.
+        </p>
+        <Button
+          className="px-6 py-3 text-white bg-secondary hover:text-secondary hover:bg-none hover:border hover:border-2px hover:border-secondary"
+          variant="ghost"
+        >
+          <Link target="_blank" href="https://forms.gle/k9scR3Q7YSPaRoUc9">
+            Click here to register
+          </Link>
+        </Button>
+      </div>
+    </MaxWidthWrapper>
+  );
+};
 
-export default page
+export default page;
