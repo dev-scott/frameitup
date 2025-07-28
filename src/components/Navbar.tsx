3f04629997c7d45567ec5abb869c5b7cab40d412
Dev-scott <sadoscott25@gmail.com>
2025-07-28 10:07:42 +0100

    feat: add video in hero section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -55,19 +55,22 @@ const Navbar = async () => {
                     )}
                   />
                 </Button> */}
-                <Link href="/configure/upload">
-                  <Button
-                    className="gap-1.5 relative  border hover:border-secondary border-gray-700 text-gray-700  rounded-sm  cursor-pointer hover:bg-secondary hover:text-white "
-                    variant={"ghost"}
-                    // disabled={true}
-                  >
-                    Create your own frame
-                    {/* <Ban
-                      className="absolute -top-[5px] -right-[8px] text-red-500 "
-                      size={30}
-                    /> */}
-                  </Button>
-                </Link>
+                {/* <Link 
+                href="/configure/upload"
+
+                > */}
+                <Button
+                  className="gap-1.5 relative  border hover:border-secondary border-gray-700 text-gray-700  rounded-sm  cursor-pointer hover:bg-secondary hover:text-white "
+                  variant={"ghost"}
+                  disabled={true}
+                >
+                  Create your own frame
+                  <Ban
+                    className="absolute -top-[5px] -right-[8px] text-red-500 "
+                    size={30}
+                  />
+                </Button>
+                {/* </Link> */}
               </div>
 
               <div className="ml-auto flex items-center">
