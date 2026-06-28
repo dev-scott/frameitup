bdf6d3c445f8d1097befff93aa7e3ea23a67e730
Dev-scott <sadoscott25@gmail.com>
2026-06-28 23:28:35 +0100

    chore:working on next.config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from 'next';
 import path from 'path';
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['@frameitup/ui', '@frameitup/types', '@frameitup/utils', '@frameitup/database'],
+  transpilePackages: ['@frameitup/ui', '@frameitup/types', '@frameitup/utils'],
+  serverExternalPackages: ['@prisma/client', '.prisma/client','@frameitup/database'],
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**.cloudflare.com' },
@@ -13,7 +14,7 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../../'),
+    // outputFileTracingRoot: path.join(__dirname, '../../'),
     serverActions: {
       bodySizeLimit: '10mb',
     }
